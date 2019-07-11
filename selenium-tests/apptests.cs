d75133277c91ec08aec5ec6602b654785114f8a8
Timothy Adams <adamst@epi.usf.edu>
2019-07-11 08:09:31 -0400

    added selenium unit test project

---
+++ selenium-tests/apptests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class Apptests
+    {
+        [TestMethod]
+        public void MatchVersionCode()
+        {
+            var browserDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //Set Chrome to start with maximized window
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+
+            //Open Chrome
+            using (var chromeDriver = new ChromeDriver(browserDriverPath, options))
+            {
+                //Assign google.com to variable named url
+                var url = "http://localhost:5000";
+                //Go to Google.com
+                chromeDriver.Navigate().GoToUrl(url);
+
+                var wait = new WebDriverWait(chromeDriver, new TimeSpan(0, 0, 1, 0));
+
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Name("ver")));
+
+                chromeDriver.Close();
+            }
+            
+            Assert.IsTrue(true);
+        }
+    }
+}
